8754ff08f4415f012cf19011cda5dd9bf7492288
ReshanCSX <reshan.luck@gmail.com>
2024-01-07 11:45:48 +0530

    Added navigation menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import Link from "next/link"
+import { Poppins } from "next/font/google"
+import { cn } from "@/lib/utils"
 import {
   Card,
   CardContent,
@@ -9,13 +10,19 @@ import {
 
 } from "@/components/ui/card"
 
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+
+})
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
       <Card className="w-1/2">
         <CardHeader>
-          <CardTitle className="text-center text-2xl text text-red-600">Hello!</CardTitle>
+          <CardTitle className={cn("text-center text-2xl text text-red-600", font.className)}>Hello!</CardTitle>
           <CardDescription className="text-center py-4">I'm Reshan, and welcome to my portfolio. I'm a Marketer and a full-stack developer, and I love to solve puzzles.</CardDescription>
         </CardHeader>
       </Card>
---
+++ components/ui/Nav.tsx
@@ -0,0 +1,39 @@
+'use client';
+import Link from "next/link";
+
+
+const navItems = [
+  {
+    name: "Home",
+    link: "/"
+  },
+  {
+    name: "About",
+    link: "/about"
+  },
+  {
+    name: "Contact",
+    link: "/contact"
+  }
+]
+
+function Nav() {
+  return (
+    <nav className="w-full border-b py-3">
+      <div className="max-w-screen-lg flex items-center justify-between mx-auto">
+        <div className="text-xl font-bold text-red-600">
+          <Link href="/">RCSX.</Link>
+        </div>
+        <div className="flex gap-2">
+          {
+            navItems.map(({ name, link }) => (
+              <Link key={link} href={link} className="rounded-full px-5 py-2 hover:bg-accent duration-300">{name}</Link>
+            ))
+          }
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
