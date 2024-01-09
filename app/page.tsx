fcb47c77f42a0ada80e5f41ae318f04aa5c8e914
ReshanCSX <reshan.luck@gmail.com>
2024-01-09 15:42:24 +0530

    Added hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,13 @@
-import { Poppins } from "next/font/google"
-import { cn } from "@/lib/utils"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+import HeroSection from "@/components/Hero"
 
-} from "@/components/ui/card"
 
-const font = Poppins({
-  subsets: ["latin"],
-  weight: ["600"],
-
-})
-
-export default function Home() {
+function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-      <Card className="w-1/2">
-        <CardHeader>
-          <CardTitle className={cn("text-center text-2xl text text-red-600", font.className)}>{'Hello!'}</CardTitle>
-          <CardDescription className="text-center py-4">{"I'm Reshan, and welcome to my portfolio. I'm a Marketer and a full-stack developer, and I love to solve puzzles."}</CardDescription>
-        </CardHeader>
-      </Card>
-
+    <main>
+      <HeroSection />
     </main>
   )
 }
+
+
+export default Home
