e28fb6fbd17c36a8571979c352bc846147d14d16
ReshanCSX <reshan.luck@gmail.com>
2024-01-10 07:03:33 +0530

    Added mobile menu

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,13 +9,13 @@ function HeroSection() {
   return (
     <section className="md:w-full py-20 flex justify-center px-5 border-b">
         <div className="max-w-screen-sm w-full flex flex-col gap-6 text-center">
-          <h1 className={`${raleway.className} text-6xl font-extrabold`}>
+          <h1 className={`${raleway.className} text-5xl sm:text-6xl font-extrabold`}>
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-green-700">{"Hi, I'm Reshan,"}</span>
           </h1>
-          <p className="text-lg text-muted-foreground">{"a full-stack developer with a design edge. I craft clean code and captivating brands that drive results for small businesses and individuals."}</p>
+          <p className="sm:text-lg text-muted-foreground">{"a full-stack developer with a design edge. I craft clean code and captivating brands that drive results for small businesses and individuals."}</p>
           <div className="mt-3">
             <Button size="lg">
-              <Link href="/contact" className="font-bold">{"Let's Collaborate On Your Next Big Thing"}</Link>
+              <Link href="/contact" className="font-bold sm:text-lg">{"Let's Collaborate On Your Next Big Thing"}</Link>
             </Button>
           </div>
         </div>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,11 @@
 'use client';
+
 import Link from "next/link";
 import { ModeToggle } from "./ui/mode-toggle";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+import { Button } from "./ui/button";
 
 
 const navItems = [
@@ -16,24 +21,52 @@ const navItems = [
     name: "Contact",
     link: "/contact"
   }
-]
+];
 
 function Nav() {
+
+  const path = usePathname();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const itemStyles = "rounded-full px-5 py-2 hover:bg-accent duration-300"; 
+
   return (
     <nav className="w-full border-b py-3">
-      <div className="max-w-screen-lg flex items-center justify-between mx-auto">
-        <div className="text-xl font-bold text-red-600">
+      <div className="max-w-screen-lg flex items-center justify-between mx-auto px-4">
+        <div className="text-xl font-bold">
           <Link href="/">RCSX.</Link>
         </div>
-        <div className="flex gap-2">
-          {
-            navItems.map(({ name, link }) => (
-              <Link key={link} href={link} className="rounded-full px-5 py-2 hover:bg-accent duration-300">{name}</Link>
-            ))
-          }
+        <div className="flex">
+
+          <div className="hidden sm:flex sm:gap-2">
+            {
+              navItems.map(({ name, link }) => (
+                <Link key={link} href={link} className={path === link ? `bg-accent shadow-inner dark:shadow-black ${itemStyles}` : `bg-transparent  ${itemStyles}`}>{name}</Link>
+              ))
+            }
+          </div>
+
+          <div className="flex sm:hidden">
+            <Button variant={"outline"} size={"icon"} className="mx-1" onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
+              { isMobileMenuOpen
+                ? <X />
+                : <Menu />
+              }
+            </Button>
+          </div>
+
           <ModeToggle />
         </div>
       </div>
+
+      { isMobileMenuOpen && 
+          <div className="pt-3">
+            {
+              navItems.map(({ name, link }) => (
+                <Link key={link} href={link} className={path === link ? "bg-accent block py-4 text-center" : "block py-4 text-center"} onClick={() => setIsMobileMenuOpen(false)}>{name}</Link>
+              ))
+            }
+          </div>
+      }
     </nav>
   )
 }
