ac691b82fc9fa3d59f715fc91a581f14581b850a
ReshanCSX <reshan.luck@gmail.com>
2024-02-07 20:43:35 +0530

    route groups to fix sanity layout

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/Hero"
+import Project from "@/components/Project"
 
 
 function Home() {
   return (
     <main>
       <HeroSection />
+      <Project />
     </main>
   )
 }
